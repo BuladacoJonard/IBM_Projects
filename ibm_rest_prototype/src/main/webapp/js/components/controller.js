9beedf9733a2dafe71931a0978dde7fe1338388d
Jonard Buladaco <Jonard Buladaco@ISD-PF00PJND.ph.ibm.com>
2016-01-11 17:05:11 +0800

    include new samples

---
+++ ibm_rest_prototype/src/main/webapp/js/components/controller.js
@@ -0,0 +1,28 @@
+var myApp = angular.module('spicyApp1', []);
+
+myApp.controller('SpicyController', ['$scope', function($scope) {
+    $scope.spice = 'very';
+
+    $scope.chiliSpicy = function() {
+        $scope.spice = 'chili';
+    };
+
+    $scope.jalapenoSpicy = function() {
+        $scope.spice = 'jalapeño';
+    };
+    
+    $scope.labuyoSpicy = function() {
+        $scope.spice = 'labuyo';
+    };
+}]);
+
+
+myApp.controller('SpicyController2', ['$scope', function($scope) {
+    $scope.customSpice = "wasabi";
+    $scope.spice = 'very';
+
+    $scope.spicy = function(spice) {
+        $scope.spice = spice;
+    };
+}]);
+
