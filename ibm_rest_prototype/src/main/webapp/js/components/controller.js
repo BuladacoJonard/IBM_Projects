db1968f2cece02fe27acd2b90b1f655284937684
Jonard Buladaco <Jonard Buladaco@ISD-PF00PJND.ph.ibm.com>
2016-01-11 19:18:40 +0800

    add new changes for angular controllers

--- ibm_rest_prototype/src/main/webapp/js/components/controller.js
+++ ibm_rest_prototype/src/main/webapp/js/components/controller.js
@@ -1,28 +1,48 @@
 var myApp = angular.module('spicyApp1', []);
 
+// Simple Controller
 myApp.controller('SpicyController', ['$scope', function($scope) {
-    $scope.spice = 'very';
+    $scope.spice      = 'very';
 
     $scope.chiliSpicy = function() {
-        $scope.spice = 'chili';
+        $scope.spice  = 'chili';
     };
 
     $scope.jalapenoSpicy = function() {
-        $scope.spice = 'jalapeño';
+        $scope.spice  = 'jalapeño';
     };
     
     $scope.labuyoSpicy = function() {
-        $scope.spice = 'labuyo';
+        $scope.spice  = 'labuyo';
     };
 }]);
 
 
+// Input Controller
 myApp.controller('SpicyController2', ['$scope', function($scope) {
     $scope.customSpice = "wasabi";
-    $scope.spice = 'very';
+    $scope.spice       = 'very';
 
     $scope.spicy = function(spice) {
-        $scope.spice = spice;
+        $scope.spice   = spice;
     };
 }]);
+
+
+// Nested Controllers
+myApp.controller('MainController', ['$scope', function($scope) {
+    $scope.timeOfDay   = 'morning';
+    $scope.name        = 'Jonard';
+}]);
+
+myApp.controller('ChildController', ['$scope', function($scope) {
+    $scope.name        = 'Jonard';
+}]);
+
+myApp.controller('GrandChildController', ['$scope', function($scope) {
+    $scope.timeOfDay   = 'evening';
+    $scope.name        = 'Jonard';
+}]);
+
+
 
